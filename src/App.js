c64cbb60ce6e7db60b4ffcb5c3d720be59b0cae5
megacheburon1995 <chebur0206@gmail.com>
2020-09-10 18:20:38 +0300

    add new project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Layout from "./hoc/Layout";
+import { Route, Switch } from "react-router-dom";
+import Quiz from "./containers/Quiz";
+import QuizList from "./containers/QuizList";
+import Auth from "./containers/Auth";
+import QuizCreator from "./containers/QuizCreator";
+
+class App extends Component {
+    render() {
+        return (
+            <Layout>
+                <Switch>
+                    <Route path='/auth' component={Auth} />
+                    <Route path='/quiz-creator' component={QuizCreator} />
+                    <Route path='/quiz/:id' component={Quiz} />
+                    <Route path='/' component={QuizList} />
+                </Switch>
+            </Layout>
+        );
+    }
+}
+
+export default App;
---
+++ src/containers/QuizCreator/index.js
@@ -0,0 +1,1 @@
+export { default } from "./QuizCreator";
